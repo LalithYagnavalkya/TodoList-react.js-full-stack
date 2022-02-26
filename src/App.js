c3b1d7cf93736cd1492c77bc9c6555b0b43f01ce
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-02-26 16:53:25 +0530

    front end done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import styledComponents from 'styled-components';
-import './App.css';
-import { Item } from './components/Item';
-
+import styled from "styled-components";
+import "./App.css";
+import { Item } from "./components/Item";
+import { Input } from "antd";
+import { Divider } from "antd";
+import { useState } from "react";
+import { Button, Radio } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const [items, setItems] = useState(["Start your daily routine"]);
+  const [input, setInput] = useState("");
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => index != id);
+    });
+  };
+
+  const updateItems = (e) => {
+    e.preventDefault();
+    if (input != "") {
+      setItems((prevItems) => {
+        return [...prevItems, input];
+      });
+      setInput("");
+    }
+  };
+
   return (
     <div className="App">
-     <h1> Todo List</h1>
-     <Wrapper>
+      <br />
+      <h1> Todo List</h1>
+      <Divider dashed />
 
-     <Item />
-     </Wrapper>
+      {items.map((i, index) => {
+        return (
+          <Item key={index} index={index} text={i} deleteid={deleteItem} />
+        );
+      })}
+
+      <form>
+        <Wrapper>
+          <Input
+            name="item"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            placeholder="type here..."
+          />
+          <Button htmlType="submit" onClick={updateItems} size={"primary"}>
+            Add
+          </Button>
+        </Wrapper>
+      </form>
     </div>
   );
 }
 
-const Wrapper = styledComponents.div`
-display: flex;
-width: auto;
-background-color: black;
-align-items: center;
+const Wrapper = styled.div`
+  display: flex;
+  width: 50%;
+  margin: auto;
+  margin-top: 10%;
+  /* margin-bottom: 70%; */
+  padding-bottom: 10%;
+`;
 
-`
 export default App;
-
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import styledComponents from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import "antd/dist/antd.css";
+import { Space, Card } from "antd";
+import { Checkbox, Button } from "antd";
 
-export const Item = () => {
+export const Item = ({ text, index, deleteid }) => {
   return (
-      <Wrapper>
-          item
-      </Wrapper>
-  )
-}
+    <Wrapper>
+      <Card className="crd">
+        <h4>{text}</h4>
+        <div>
+          <Checkbox
+            className="check"
+            onChange={(e) => {
+              e.target.checked = true;
+            }}
+          ></Checkbox>
+          <Button
+            onClick={() => {
+              deleteid(index);
+            }}
+            type="primary"
+            shape="round"
+            size="small"
+          >
+            delete
+          </Button>
+        </div>
+      </Card>
+    </Wrapper>
+  );
+};
 
-const Wrapper = styledComponents.div`
-background-color: yellow;
-width: 50%;
-
-`
+const Wrapper = styled.div`
+  .crd {
+    margin: auto;
+    div {
+      display: inline;
+    }
+    h4 {
+      display: inline;
+    }
+    margin-top: 2%;
+    width: 80%;
+    .check {
+      margin-left: 10px;
+      margin-right: 10px;
+    }
+  }
+`;
