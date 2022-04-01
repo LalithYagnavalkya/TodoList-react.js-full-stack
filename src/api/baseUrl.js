2059f6e9ff7cd379129128969b1e6009cc37bee5
Lalith Yagnavalkya <69410566+LalithYagnavalkya@users.noreply.github.com>
2022-04-01 18:09:36 +0530

    Update baseUrl.js

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseUrl = axios.create({
   baseURL:
-    "https://cors-anywhere.herokuapp.com/https://stormy-ravine-46879.herokuapp.com/api",
+    "https://stormy-ravine-46879.herokuapp.com/api",
 });
 
 export default baseUrl;
