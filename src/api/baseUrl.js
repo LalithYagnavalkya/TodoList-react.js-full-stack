54d317e7572bd0416b3d8fe50d3c06f2cd09e68c
Lalith Yagnavalkya <lalluyagnavalkya.t@gmail.com>
2022-04-01 11:03:51 +0530

    add cors

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseUrl = axios.create({
-  baseURL: "https://stormy-ravine-46879.herokuapp.com/api",
+  baseURL:
+    "https://cors-anywhere.herokuapp.com/https://stormy-ravine-46879.herokuapp.com/api",
 });
 
 export default baseUrl;
